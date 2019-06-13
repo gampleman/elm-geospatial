e74bb8baa502a2f87b8b895c1ab1a4f3134c77c5
Jakub Hampl <kopomir@gmail.com>
2019-06-13 16:45:15 +0100

    Fix issue

--- examples/index.js
+++ examples/index.js
@@ -1,13 +1,10 @@
 import { Elm } from "./src/Main.elm";
 import { registerCustomElement, registerPorts } from "elm-mapbox";
-import {register} from 'elm-debug-transformer';
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const token =
     "pk.eyJ1IjoiZm9yZXN0LXByb3RlY3Rvci1nYW1lIiwiYSI6ImNqdnMxMmE4aTBtM3A0YW1zc2gxaDZwMTkifQ.5GqU-G9npYW3UeCxQYrV8g";
-
-register();
 
 registerCustomElement({
     token
